811f6d038844b89328ece818240fd0a0514e8d25
Vladthe49th <155636142+Vladthe49th@users.noreply.github.com>
2025-12-11 14:22:09 +0200

    Delete src/Imagebox directory

--- src/Imagebox/Imagebox.jsx
+++
@@ -1,9 +0,0 @@
-import "./Imagebox.css";
-
-export default function ImageBox({ src }) {
-    return (
-        <div className="img-box">
-            <img src={src} alt="demo" />
-        </div>
-    );
-}
