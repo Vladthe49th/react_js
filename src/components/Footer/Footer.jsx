9396c21b895bc79e45abe06776f7d80609e82c7b
Vladthe49th <155636142+Vladthe49th@users.noreply.github.com>
2025-12-19 11:51:48 +0200

    Delete src/components directory

--- src/components/Footer/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-    return <footer>© 2025. Рецепт зроблено з душею та чистими руками</footer>;
-}
--- src/components/StepsList/StepItem.jsx
+++
@@ -1,3 +0,0 @@
-export default function StepItem({  text }) {
-    return <li><strong>{text}</strong> </li>;
-}
